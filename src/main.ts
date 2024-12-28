7757d472a9d7b8aaca3974d8582c99da285adac8
thursdaycurry <thursdaycurry@gmail.com>
2024-12-28 20:06:30 +0900

    fix type error

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { CollageMaker } from "./classes/CollageMaker";
 import "./style.css";
 
-const uploadedImages: HTMLElement | null =
-  document.getElementById("uploadedImages");
+const uploadedImages = document.getElementById(
+  "uploadedImages"
+) as HTMLInputElement | null;
+
 const downloadButton: HTMLElement | null =
   document.getElementById("downloadPdf");
 
 if (uploadedImages && downloadButton) {
   downloadButton.addEventListener("click", async (): Promise<void> => {
-    const images = uploadedImages.files;
+    const images: FileList | null = uploadedImages.files;
 
-    const collage = new CollageMaker("a1", images);
-    await collage.drawCollageOutline().drawCollageGrid().drawCollageImages();
+    if (images && images.length > 0) {
+      const imageArray: File[] = Array.from(images);
 
-    const pdf = collage.getPDF();
+      const collage = new CollageMaker("a1", imageArray);
+      await collage.drawCollageOutline().drawCollageGrid().drawCollageImages();
 
-    pdf.save("collage.pdf");
+      const pdf = collage.getPDF();
+
+      pdf.save("collage.pdf");
+    }
   });
 }
